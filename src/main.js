2042ebecd226d89748c0dbe7f69a4118217f9460
kim_chen <yyahaowan@163.com>
2024-05-13 22:10:28 +0800

    更新web-server

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n'); 
+  const data={
+    id:1,
+    title:'关山月',
+    content:"明月出天山，苍茫云海间。"
+  }
+
+   
+  res.writeHead(200,{
+    'Access-Control-Allow-Origin':'*',
+    'Content-Type':'application/json;charset=utf-8'
+  });
+  res.end(JSON.stringify(data)); 
 });
 
 app.listen(3000, () => {
