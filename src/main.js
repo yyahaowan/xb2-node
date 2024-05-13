b697037bae11e6b6c15410ad62757eb2e78200e2
kim_chen <yyahaowan@163.com>
2024-05-13 19:49:13 +0800

    创建 Web 服务器

--- src/main.js
+++ src/main.js
@@ -1,2 +1,11 @@
-const greeting ="你好222333~~~"
-console.log(greeting);
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n'); 
+});
+
+app.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+})
